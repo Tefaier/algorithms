e3cb94d9400b5977be4f948989938559fa198138
Tefaier <Tefaier@gmail.com>
2023-10-22 15:49:49 +0300

    Task H - 9 fails due to memory limit

--- src/main/java/Task01_H/Main.java
+++ src/main/java/Task01_H/Main.java
@@ -1,31 +1,38 @@
 package Task01_H;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
   // stat K starts from 1, returns value of element that will be at this position
-  public static int getStatK(int[] arr, int k) {
-    if (arr.length == 2) {
-      return k == 1 ? Math.min(arr[0], arr[1]) : Math.max(arr[0], arr[1]);
-    } else if (arr.length == 1) {
-      return arr[0];
+  // from starts inclusive, to end inclusive
+  public static int getStatK(int[] arr, int k, int from, int to) {
+    if (to - from == 1) {
+      // == to because k is index + 1 and here to = from + 1, so if != to then it's k = to + 1 ONLY
+      return k == to ? Math.min(arr[from], arr[to]) : Math.max(arr[from], arr[to]);
+    } else if (from == to) {
+      return arr[from];
     } else {
-      int pivotPos = partition(arr, 0, arr.length - 1);
+      // int pivot = getMiddle(Arrays.copyOfRange(arr, from, to + 1));
+      int pivot = getMiddle(arr, from, to);
+      // element including which after all >= pivot
+      int pivotPos = partition(arr, pivot, from, to);
+      // element including which after all > pivot
+      int pivotsEnd = partition(arr, pivot + 1, pivotPos, to);
       if (k <= pivotPos) {
-        return getStatK(Arrays.copyOfRange(arr, 0, pivotPos), k);
+        return getStatK(arr, k, from, pivotPos - 1);
+      } else if (k <= pivotsEnd) {
+        return pivot;
       } else {
-        return getStatK(Arrays.copyOfRange(arr, pivotPos, arr.length), k - pivotPos);
+        return getStatK(arr, k, pivotsEnd, to);
       }
     }
   }
 
   // returns index at which pivot is (to the left all <, to the right (including) all >=)
   // also changes provided arr
-  public static int partition(int[] arr, int from, int to) {
-    int pivot = getMiddle(Arrays.copyOfRange(arr, from, to + 1));
-
+  public static int partition(int[] arr, int pivot, int from, int to) {
     int i = (from - 1);
     for (int j = from; j <= to; j++) {
       if (arr[j] < pivot) {
@@ -37,29 +44,27 @@ public class Main {
     return (i + 1);
   }
 
-  public static int getMiddle(int[] arr) {
-    int[] middles = getFivesMiddle(arr);
-    return getStatK(middles, middles.length / 2 + 1);
-  }
-
-  public static int[] getFivesMiddle(int[] arr) {
-    int[] answer = new int[(int) Math.ceil(arr.length / 5f)];
-    for (int i = 0; i < arr.length; i += 5) {
-      answer[i / 5] = getMedium(arr, i, i + 4);
+  // from and to inclusive
+  public static int getMiddle(int[] arr, int from, int to) {
+    // int[] answer = new int[(int) Math.ceil((to + 1 - from) / 5f)];
+    ArrayList<Integer> answer = new ArrayList<>();
+    for (int i = from; i <= to; i += 5) {
+      answer.add(getMedium(arr, i, Math.min(i + 4, to)));
+      // answer[i / 5] = getMedium(arr, i, i + 4);
     }
-    return answer;
+    int mid = (int) Math.ceil(answer.size() / 2f);
+    return getStatK(answer.stream().mapToInt(i -> i).toArray(), mid, 0, answer.size() - 1);
   }
 
   public static int getMedium(int[] arr, int from, int to) {
-    int limit = Math.min(to + 1, arr.length);
-    int[] unit = Arrays.copyOfRange(arr, from, limit);
-    bubbleSort(unit);
-    return unit[unit.length / 2];
+    // int limit = Math.min(to + 1, arr.length);
+    bubbleSort(arr, from, to);
+    return arr[(to + from) / 2];
   }
 
-  public static void bubbleSort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = i + 1; j < arr.length; j++) {
+  public static void bubbleSort(int[] arr, int from, int to) {
+    for (int i = from; i <= to; i++) {
+      for (int j = i + 1; j <= to; j++) {
         if (arr[i] > arr[j]) {
           swap(arr, i, j);
         }
@@ -90,6 +95,6 @@ public class Main {
       array[i] = (array[i - 1] * 123 + array[i - 2] * 45) % mod;
     }
 
-    System.out.println(getStatK(array, stat));
+    System.out.println(getStatK(array, stat, 0, array.length - 1));
   }
 }
