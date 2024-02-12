9d3df8c92813703d2ab00b0d19ea8b5e0c869960
Tefaier <Tefaier@gmail.com>
2024-02-12 19:52:10 +0300

    B - success

--- src/main/java/Task05_B/Main.java
+++ src/main/java/Task05_B/Main.java
@@ -3,9 +3,6 @@ package Task05_B;
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
   static class Parser {
@@ -104,158 +101,60 @@ public class Main {
     }
   }
 
-  static enum VertexStatus {White, Gray, Black}
-
-  static class DFS<V, E extends GraphExplorer<V>> {
-    public final Graph<V> graph;
-    public final E explorer;
-
-    public DFS(Graph<V> graph, E graphExplorer) {
-      this.graph = graph;
-      this.explorer = graphExplorer;
-    }
-
-    public void initExplorer() {
-      explorer.prepareGraph(graph);
-    }
-
-    public void startDFS() {
-      V vertex = explorer.getRandomUnexplored();
-      explorer.startExploring(vertex);
-      dfs(vertex);
-    }
-
-    public void startDFS(V vertex) {
-      explorer.startExploring(vertex);
-      dfs(vertex);
-    }
-
-    public void dfs(V vertex) {
-      explorer.setVertexStatus(vertex, VertexStatus.Gray);
-      explorer.startVertex(vertex);
-      for (V next : graph.getConnected(vertex)) {
-        if (explorer.getVertexStatus(next) == VertexStatus.White) {
-          dfs(next);
-        }
-      }
-      explorer.setVertexStatus(vertex, VertexStatus.Black);
-    }
-  }
-
-  static interface GraphExplorer<V> {
-    public V getRandomUnexplored();
-
-    public void prepareGraph(Graph<V> graph);
-
-    public VertexStatus getVertexStatus(V vertex);
-
-    public void setVertexStatus(V vertex, VertexStatus status);
-
-    public void startExploring(V vertex);
-
-    public void startVertex(V vertex);
-
-    public void exploreOld(V vertex);
-
-    public void exploreFinished(V vertex);
-
-    public void endVertex(V vertex);
-
-    public boolean isFinished();
-  }
-
-  static class Graph<V> {
-    public HashMap<V, Set<V>> connectionList = new HashMap<>();
-    public List<V> vertexes;
-
-    public Graph() {
-
-    }
-
-    public Graph(List<Edge<V>> edges, List<V> vertexes, boolean orientated) {
-      this.vertexes = vertexes;
-      vertexes.forEach(vert -> connectionList.put(vert, new HashSet<>()));
-      for (Edge<V> edge : edges) {
-        connectionList.get(edge.from()).add(edge.to());
-        if (!orientated) {
-          connectionList.get(edge.to()).add(edge.from());
-        }
-      }
-    }
-
-    public Set<V> getConnected(V vertex) {
-      return connectionList.get(vertex);
-    }
-  }
-
-  static record Edge<V>(V from, V to) {
-  }
-
-  static class ClustersSearch<V> implements GraphExplorer<V> {
+  // code base taken from https://tutorialhorizon.com/algorithms/count-number-of-subgraphs-in-a-given-graph/
+  static class Graph {
+    int vertices;
+    List<Integer>[] adjList;
     public StringBuilder answer = new StringBuilder();
-    public StringBuilder lastCluster;
-    public int lastClusterSize = 0;
-    public int clusterNumber = 0;
+    private StringBuilder currentCluster;
+    private int sizeCounter;
+    private int checkedPoint = 0;
 
-    private Set<V> unexplored = new HashSet<>();
-    private HashMap<V, VertexStatus> vertexStatuses = new HashMap<>();
 
-    @Override
-    public V getRandomUnexplored() {
-      return unexplored.isEmpty() ? null : unexplored.stream().findAny().get();
-    }
-
-    @Override
-    public void prepareGraph(Graph<V> graph) {
-      unexplored.addAll(graph.connectionList.keySet());
-    }
-
-    @Override
-    public VertexStatus getVertexStatus(V vertex) {
-      return vertexStatuses.getOrDefault(vertex, VertexStatus.White);
-    }
-
-    @Override
-    public void setVertexStatus(V vertex, VertexStatus status) {
-      vertexStatuses.put(vertex, status);
-    }
-
-    @Override
-    public void startExploring(V vertex) {
-      if (lastClusterSize != 0) {
-        answer.append(lastClusterSize).append('\n').append(lastCluster).append('\n');
+    public Graph(int vertices) {
+      this.vertices = vertices + 1;
+      adjList = new List[vertices + 1];
+      for (int i = 0; i < vertices + 1; i++) {
+        adjList[i] = new ArrayList<>();
       }
-      clusterNumber++;
-      lastClusterSize = 0;
-      lastCluster = new StringBuilder();
-      vertexStatuses = new HashMap<>();
     }
 
-    @Override
-    public void startVertex(V vertex) {
-      lastClusterSize++;
-      lastCluster.append(vertex).append(" ");
-      unexplored.remove(vertex);
+    public void addEdge(int source, int destination) {
+      adjList[source].add(destination);
+      adjList[destination].add(source);
     }
 
-    @Override
-    public void exploreOld(V vertex) {
-      return;
+
+    public int countConnectedComponents() {
+      boolean[] visited = new boolean[vertices];
+      int count = 0;
+      int index;
+      while ((index = checkGraphIsVisited(visited)) != -1) {
+        currentCluster = new StringBuilder();
+        sizeCounter = 0;
+        dfs(index, visited);
+        count++;
+        answer.append(sizeCounter).append('\n').append(currentCluster).append('\n');
+        checkedPoint = index;
+      }
+      return count;
     }
 
-    @Override
-    public void exploreFinished(V vertex) {
-      return;
+    public int checkGraphIsVisited(boolean[] visited) {
+      for (int i = checkedPoint + 1; i < visited.length; i++) {
+        if (!visited[i])
+          return i;
+      }
+      return -1;
     }
 
-    @Override
-    public void endVertex(V vertex) {
-      return;
-    }
-
-    @Override
-    public boolean isFinished() {
-      return unexplored.isEmpty();
+    public void dfs(int start, boolean[] visited) {
+      sizeCounter++;
+      currentCluster.append(start).append(' ');
+      visited[start] = true;
+      for (var next : adjList[start]) {
+        if (!visited[next]) dfs(next, visited);
+      }
     }
   }
 
@@ -263,20 +162,11 @@ public class Main {
     Parser in = new Parser(System.in);
     int vertexes = in.nextInt();
     int edges = in.nextInt();
-    List<Integer> vertexesList = IntStream.rangeClosed(1, vertexes).boxed().toList();
-    List<Edge<Integer>> edgesList = new ArrayList<>();
+    Graph graph = new Graph(vertexes);
     for (int i = 0; i < edges; i++) {
-      edgesList.add(new Edge<>(in.nextInt(), in.nextInt()));
+      graph.addEdge(in.nextInt(), in.nextInt());
     }
-    Graph<Integer> graph = new Graph<>(edgesList, vertexesList, false);
-    ClustersSearch<Integer> clustersSearch = new ClustersSearch<>();
-    DFS<Integer, ClustersSearch<Integer>> dfs = new DFS<>(graph, clustersSearch);
-    dfs.initExplorer();
-    while (!clustersSearch.isFinished()) {
-      dfs.startDFS();
-    }
-    System.out.println(clustersSearch.clusterNumber);
-    clustersSearch.answer.append(clustersSearch.lastClusterSize).append('\n').append(clustersSearch.lastCluster);
-    System.out.println(clustersSearch.answer);
+    System.out.println(graph.countConnectedComponents());
+    System.out.print(graph.answer);
   }
 }
