21b2f3e6ca9ec7341da8697ec9d4c1e430bad987
Tefaier <Tefaier@gmail.com>
2024-02-29 20:29:13 +0300

    06_E - nope

--- src/main/java/Task06_E/Main.java
+++ src/main/java/Task06_E/Main.java
@@ -2,6 +2,9 @@ package Task06_E;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Parser {
 
@@ -100,7 +103,48 @@ class Parser {
 }
 
 public class Main {
+  private static Parser in = new Parser(System.in);
+  private static int inf = Integer.MAX_VALUE;
+
+  private static List<Integer> locateCycleOfNW(int[][] graphMatrix) {
+    int[] dist = new int[graphMatrix.length];
+    Arrays.fill(dist, inf);
+
+    // shows vertexes that were suspicious in iteration
+    ArrayList<Integer> black_list = new ArrayList<>();
+    // pointer shows first not analyzed vertex
+    int pointer = 0;
+    while (black_list.isEmpty() && pointer < graphMatrix.length) {
+      while (pointer < graphMatrix.length && dist[pointer] != inf) pointer++;
+      if (pointer == graphMatrix.length) break;
+
+      dist[pointer] = 0;
+      for (int k = 0; k < graphMatrix.length; k++) {
+        for (int i = 0; i < m; i++) {
+          int f = edges[i].from;
+          int t = edges[i].to;
+          long w = edges[i].w;
+          if (dist[f] == inf) continue;
+          if (dist[t] > dist[f] + w) {
+            dist[t] = dist[f] + w;
+            if (k == graphMatrix.length - 1) {
+              black_list.add(t);
+            }
+          }
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     Parser in = new Parser(System.in);
+    int gSize = in.nextInt();
+    int[][] graph = new int[gSize][gSize];
+    for (int i = 0; i < gSize; i++) {
+      for (int j = 0; j < gSize; j++) {
+        graph[i][j] = in.nextInt();
+      }
+    }
+    var result = locateCycleOfNW(graph);
   }
 }
