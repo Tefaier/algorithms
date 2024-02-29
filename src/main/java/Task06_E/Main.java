65391df4024b56ab734d9743ec87b17680506389
Tefaier <Tefaier@gmail.com>
2024-02-29 21:18:36 +0300

    06_E - success

--- src/main/java/Task06_E/Main.java
+++ src/main/java/Task06_E/Main.java
@@ -102,37 +102,67 @@ class Parser {
   }
 }
 
+record Edge(int from, int to, int weight) {
+}
+
 public class Main {
   private static Parser in = new Parser(System.in);
   private static int inf = Integer.MAX_VALUE;
 
-  private static List<Integer> locateCycleOfNW(int[][] graphMatrix) {
+  private static void locateCycleOfNW(int[][] graphMatrix, List<Edge> edges) {
+    int[] parent = new int[graphMatrix.length];
+    Arrays.fill(parent, -1);
     int[] dist = new int[graphMatrix.length];
-    Arrays.fill(dist, inf);
+    boolean[] checked = new boolean[graphMatrix.length];
+    Arrays.fill(checked, false);
 
     // shows vertexes that were suspicious in iteration
     ArrayList<Integer> black_list = new ArrayList<>();
     // pointer shows first not analyzed vertex
     int pointer = 0;
     while (black_list.isEmpty() && pointer < graphMatrix.length) {
-      while (pointer < graphMatrix.length && dist[pointer] != inf) pointer++;
+      while (pointer < graphMatrix.length && checked[pointer]) pointer++;
       if (pointer == graphMatrix.length) break;
 
+      Arrays.fill(dist, inf);
       dist[pointer] = 0;
+      checked[pointer] = true;
       for (int k = 0; k < graphMatrix.length; k++) {
-        for (int i = 0; i < m; i++) {
-          int f = edges[i].from;
-          int t = edges[i].to;
-          long w = edges[i].w;
-          if (dist[f] == inf) continue;
-          if (dist[t] > dist[f] + w) {
-            dist[t] = dist[f] + w;
+        boolean relaxed = false;
+        for (int i = 0; i < edges.size(); i++) {
+          if (dist[edges.get(i).from()] == inf) continue;
+          if (dist[edges.get(i).to()] > dist[edges.get(i).from()] + edges.get(i).weight()) {
+            relaxed = true;
+            checked[edges.get(i).to()] = true;
+            dist[edges.get(i).to()] = dist[edges.get(i).from()] + edges.get(i).weight();
+            parent[edges.get(i).to()] = edges.get(i).from();
             if (k == graphMatrix.length - 1) {
-              black_list.add(t);
+              black_list.add(edges.get(i).to());
             }
           }
         }
+        if (!relaxed) break;
       }
+    }
+    // finished, either all checked either cycle detected
+    if (black_list.isEmpty()) {
+      System.out.println("NO");
+      return;
+    }
+    // cycle detected
+    System.out.println("YES");
+    List<Integer> cycleList = new ArrayList<>();
+    int last = black_list.get(0);
+    cycleList.add(last);
+    while (!cycleList.contains(parent[last])) {
+      cycleList.add(parent[last]);
+      last = parent[last];
+    }
+    int indexOfCycleStart = cycleList.indexOf(parent[last]);
+    System.out.println(cycleList.size() - indexOfCycleStart + 1);
+    System.out.print(parent[last] + 1 + " ");
+    for (int i = cycleList.size() - 1; i >= indexOfCycleStart; --i) {
+      System.out.print(cycleList.get(i) + 1 + " ");
     }
   }
 
@@ -140,11 +170,15 @@ public class Main {
     Parser in = new Parser(System.in);
     int gSize = in.nextInt();
     int[][] graph = new int[gSize][gSize];
+    List<Edge> edges = new ArrayList<>();
     for (int i = 0; i < gSize; i++) {
       for (int j = 0; j < gSize; j++) {
         graph[i][j] = in.nextInt();
+        if (graph[i][j] != 100000) {
+          edges.add(new Edge(i, j, graph[i][j]));
+        }
       }
     }
-    var result = locateCycleOfNW(graph);
+    locateCycleOfNW(graph, edges);
   }
 }
