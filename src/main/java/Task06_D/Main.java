8eb03ba1f84429d23a58cfd462b7183087732170
Tefaier <Tefaier@gmail.com>
2024-02-29 20:03:42 +0300

    06_D - success

--- src/main/java/Task06_D/Main.java
+++ src/main/java/Task06_D/Main.java
@@ -2,6 +2,10 @@ package Task06_D;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Parser {
 
@@ -99,8 +103,63 @@ class Parser {
   }
 }
 
+record Edge(int from, int to, int weight) {
+}
+
+class Graph {
+  private int vertexCount;
+  private ArrayList<Edge> edges;
+
+  public Graph(int vertexCount) {
+    this.vertexCount = vertexCount;
+    edges = new ArrayList<>();
+  }
+
+  public int getVertexCount() {
+    return vertexCount;
+  }
+
+  public void addEdge(Edge edge) {
+    edges.add(edge);
+  }
+
+  public List<Edge> getEdges() {
+    return edges;
+  }
+}
+
 public class Main {
+  private static Parser in = new Parser(System.in);
+  private static int infinity = 30000;
+
+  private static int[] fordBelman(int start, Graph graph) {
+    int[] distances = new int[graph.getVertexCount()];
+    Arrays.fill(distances, infinity);
+    distances[start] = 0;
+
+    for (int k = 0; k < graph.getVertexCount(); k++) {
+      boolean relaxed = false;
+      for (Edge edge : graph.getEdges()) {
+        if (distances[edge.from()] != infinity) {
+          if (distances[edge.to()] > distances[edge.from()] + edge.weight()) {
+            distances[edge.to()] = distances[edge.from()] + edge.weight();
+            relaxed = true;
+          }
+        }
+      }
+      if (!relaxed) break;
+    }
+
+    return distances;
+  }
+
   public static void main(String[] args) {
-    Parser in = new Parser(System.in);
+    int roomNum = in.nextInt();
+    int edgeNum = in.nextInt();
+    Graph graph = new Graph(roomNum);
+    for (int j = 0; j < edgeNum; j++) {
+      graph.addEdge(new Edge(in.nextInt() - 1, in.nextInt() - 1, in.nextInt()));
+    }
+    System.out.println(Arrays.stream(fordBelman(0, graph)).mapToObj(Integer::toString).collect(Collectors.joining(" ")));
   }
 }
