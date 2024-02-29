7d7d81788bb5e16961f414030bcb9bc9e0194b48
Tefaier <Tefaier@gmail.com>
2024-02-29 23:31:16 +0300

    06_F - base

--- src/main/java/Task06_F/Main.java
+++ src/main/java/Task06_F/Main.java
@@ -2,6 +2,10 @@ package Task06_F;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Parser {
 
@@ -99,8 +103,101 @@ class Parser {
   }
 }
 
+record Edge(int from, int to, int weight) {
+}
+
+class Graph {
+  private int vertexCount;
+  private ArrayList<Edge> edges;
+
+  public Graph(int vertexCount) {
+    this.vertexCount = vertexCount;
+    edges = new ArrayList<>();
+  }
+
+  public int getVertexCount() {
+    return vertexCount;
+  }
+
+  public void addEdge(Edge edge) {
+    edges.add(edge);
+  }
+
+  public List<Edge> getEdges() {
+    return edges;
+  }
+}
+
 public class Main {
+  private static Parser in = new Parser(System.in);
+  private static int infinity = 10000000;
+  private static int negInf = infinity * -1;
+
+  private static List<Integer> flightPathByFloid(Graph graph, int[] townSeq) {
+    int size = graph.getVertexCount();
+    int[][] matrix = new int[size][size];
+    int[][] nextMatrix = new int[size][size];
+    for (int[] row : matrix) {
+      Arrays.fill(row, negInf);
+    }
+    for (int[] row : nextMatrix) {
+      Arrays.fill(row, -1);
+    }
+    for (int i = 0; i < size; i++) {
+      matrix[i][i] = 0;
+    }
+    for (Edge edge : graph.getEdges()) {
+      matrix[edge.from()][edge.to()] = Math.max(matrix[edge.from()][edge.to()], edge.weight());
+      nextMatrix[edge.from()][edge.to()] = edge.from();
+    }
+
+    // create APSP
+    int newValue;
+    for (int add = 0; add < size; ++add) {
+      for (int from = 0; from < size; ++from) {
+        for (int to = 0; to < size; ++to) {
+          newValue = matrix[from][add] + matrix[add][to];
+          if (newValue < infinity && newValue > matrix[from][to]) {
+            matrix[from][to] = newValue;
+            nextMatrix[from][to] = nextMatrix[from][add];
+          }
+        }
+      }
+    }
+
+    List<Integer> path = new ArrayList<>();
+    int index = 0;
+    int currentTown = townSeq[0];
+    while (index < townSeq.length - 2) {
+      while (currentTown != townSeq[index + 1]) {
+        if (matrix[currentTown][currentTown] > 0) return new ArrayList<>();
+        path.add(currentTown);
+        currentTown = nextMatrix[currentTown][townSeq[index + 1]];
+      }
+      ++index;
+    }
+    path.add(currentTown);
+    return path;
+  }
+
   public static void main(String[] args) {
-    Parser in = new Parser(System.in);
+    int townsNum = in.nextInt();
+    int edgeNum = in.nextInt();
+    int lecturesNum = in.nextInt();
+    Graph graph = new Graph(townsNum);
+    for (int j = 0; j < edgeNum; j++) {
+      graph.addEdge(new Edge(in.nextInt() - 1, in.nextInt() - 1, in.nextInt()));
+    }
+    int[] lectureTowns = new int[lecturesNum];
+    for (int i = 0; i < lecturesNum; i++) {
+      lectureTowns[i] = in.nextInt() - 1;
+    }
+    var result = flightPathByFloid(graph, lectureTowns);
+    if (result.isEmpty()) {
+      System.out.println("infinitely kind");
+      return;
+    }
+    System.out.println(result.size());
+    System.out.println(result.stream().map(town -> Integer.toString(town + 1)).collect(Collectors.joining(" ")));
   }
 }
