707efbe4cd41c32a9a7314d03fb1c25d45a34405
Tefaier <Tefaier@gmail.com>
2024-03-03 18:58:37 +0300

    06_A - changes for review

--- src/main/java/Task06_A/Main.java
+++ src/main/java/Task06_A/Main.java
@@ -2,10 +2,9 @@ package Task06_A;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Parser {
 
@@ -103,60 +102,135 @@ class Parser {
   }
 }
 
-record Edge(int from, int to, int weight) {
+interface Edge<V> {
+  V from();
+
+  V to();
 }
 
-class Graph {
-  private int vertexCount;
-  private ArrayList<Edge> edges;
+class WeightedEdge<V> implements Edge<V> {
+  private V from;
+  private V to;
+  private int weight;
 
-  public Graph(int vertexCount) {
-    this.vertexCount = vertexCount;
-    edges = new ArrayList<>();
+  public WeightedEdge(V from, V to, int weight) {
+    this.from = from;
+    this.to = to;
+    this.weight = weight;
+  }
+
+  @Override
+  public V from() {
+    return from;
+  }
+
+  @Override
+  public V to() {
+    return to;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+}
+
+class Graph<V, E extends Edge<V>> {
+  private int edgesNum = 0;
+  private List<V> vertexes;
+  private HashMap<V, List<E>> edgesMap = new HashMap<>();
+
+  public Graph(List<V> vertexes, List<E> edges) {
+    this.vertexes = vertexes;
+    for (E edge : edges) {
+      ++edgesNum;
+      edgesMap.putIfAbsent(edge.from(), new ArrayList<>());
+      edgesMap.get(edge.from()).add(edge);
+    }
+  }
+
+  public List<E> getConnected(V vertex) {
+    return edgesMap.getOrDefault(vertex, new ArrayList<>());
   }
 
   public int getVertexCount() {
-    return vertexCount;
+    return vertexes.size();
   }
 
-  public void addEdge(Edge edge) {
-    edges.add(edge);
+  public int getEdgeCount() {
+    return edgesNum;
   }
 
-  public List<Edge> getEdges() {
-    return edges;
+  public List<V> getVertexes() {
+    return vertexes;
+  }
+}
+
+interface DijkstraVisitor<V, E extends Edge<V>> {
+  public void instantiateVertex(V vertex);
+
+  public void discoverVertex(V vertex, Integer distance);
+}
+
+class DijkstraVisitorDistances<V, E extends Edge<V>> implements DijkstraVisitor<V, E> {
+  private static int infinity = 2009000999;
+  private HashMap<V, Integer> distances = new HashMap<>();
+
+  public DijkstraVisitorDistances(Graph<V, E> graph) {
+    graph.getVertexes().forEach(this::instantiateVertex);
+  }
+
+  @Override
+  public void instantiateVertex(V vertex) {
+    distances.put(vertex, infinity);
+  }
+
+  @Override
+  public void discoverVertex(V vertex, Integer distance) {
+    distances.put(vertex, distance);
+  }
+
+  public HashMap<V, Integer> getDistances() {
+    return distances;
   }
 }
 
 public class Main {
   private static Parser in = new Parser(System.in);
-  private static int infinity = 2009000999;
 
-  private static int[] fordBelman(int start, Graph graph) {
-    int[] distances = new int[graph.getVertexCount()];
-    Arrays.fill(distances, infinity);
-    distances[start] = 0;
+  private static <V, E extends WeightedEdge<V>, D extends DijkstraVisitor<V, E>> void dijkstra(Graph<V, E> graph, V start, D visitor) {
+    Set<V> checked = new HashSet<>();
+    class Unit implements Comparable {
+      public V vertex;
+      public int distance;
 
-    for (int k = 0; k < graph.getVertexCount(); k++) {
-      boolean relaxed = false;
-      for (Edge edge : graph.getEdges()) {
-        if (distances[edge.from()] != infinity) {
-          if (distances[edge.to()] > distances[edge.from()] + edge.weight()) {
-            distances[edge.to()] = distances[edge.from()] + edge.weight();
-            relaxed = true;
-          }
+      public Unit(V vertex, int distance) {
+        this.vertex = vertex;
+        this.distance = distance;
+      }
+
+      @Override
+      public int compareTo(Object o) {
+        if (o.getClass().isNestmateOf(Unit.class)) {
+          return distance - ((Unit) o).distance;
         }
-        if (distances[edge.to()] != infinity) {
-          if (distances[edge.from()] > distances[edge.to()] + edge.weight()) {
-            distances[edge.from()] = distances[edge.to()] + edge.weight();
-            relaxed = true;
-          }
+        return 0;
+      }
+    }
+    PriorityQueue<Unit> queue = new PriorityQueue<>();
+    queue.add(new Unit(start, 0));
+
+    Unit unit;
+    while ((unit = queue.poll()) != null) {
+      if (checked.contains(unit.vertex)) continue;
+      checked.add(unit.vertex);
+      visitor.discoverVertex(unit.vertex, unit.distance);
+
+      for (WeightedEdge<V> edge : graph.getConnected(unit.vertex)) {
+        if (!checked.contains(edge.to())) {
+          queue.add(new Unit(edge.to(), unit.distance + edge.getWeight()));
         }
       }
-      if (!relaxed) break;
     }
-
-    return distances;
   }
 
   public static void main(String[] args) {
@@ -164,11 +238,23 @@ public class Main {
     for (int i = 0; i < tries; i++) {
       int roomNum = in.nextInt();
       int edgeNum = in.nextInt();
-      Graph graph = new Graph(roomNum);
+      List<WeightedEdge<Integer>> edges = new ArrayList<>();
       for (int j = 0; j < edgeNum; j++) {
-        graph.addEdge(new Edge(in.nextInt(), in.nextInt(), in.nextInt()));
+        int v1 = in.nextInt();
+        int v2 = in.nextInt();
+        int weight = in.nextInt();
+        edges.add(new WeightedEdge<>(v1, v2, weight));
+        edges.add(new WeightedEdge<>(v2, v1, weight));
       }
-      System.out.println(Arrays.stream(fordBelman(in.nextInt(), graph)).mapToObj(Integer::toString).collect(Collectors.joining(" ")));
+      Graph<Integer, WeightedEdge<Integer>> graph = new Graph<>(IntStream.rangeClosed(0, roomNum - 1).boxed().toList(), edges);
+      DijkstraVisitorDistances<Integer, WeightedEdge<Integer>> visitor = new DijkstraVisitorDistances<>(graph);
+      dijkstra(graph, in.nextInt(), visitor);
+      System.out.println(
+          visitor.getDistances().entrySet().stream()
+              .sorted(Comparator.comparingInt(Map.Entry::getKey))
+              .map(entry -> entry.getValue().toString())
+              .collect(Collectors.joining(" "))
+      );
     }
   }
 }
