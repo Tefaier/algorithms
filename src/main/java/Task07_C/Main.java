5f655a5736782350160cb2b05b6926f6494cfb03
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-03-14 08:52:28 +0300

    07_C - success

--- src/main/java/Task07_C/Main.java
+++ src/main/java/Task07_C/Main.java
@@ -2,6 +2,10 @@ package Task07_C;
 
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.PriorityQueue;
 
 class Parser {
 
@@ -99,10 +103,110 @@ class Parser {
   }
 }
 
+class DSU {
+  private int size;
+  private int active;
+
+  private int[] linkList;
+  private int[] sizeList;
+
+  public DSU(int size) {
+    this.size = size;
+    active = size;
+    linkList = new int[size];
+    sizeList = new int[size];
+    for (int i = 0; i < size; i++) {
+      linkList[i] = i;
+      sizeList[i] = 1;
+    }
+  }
+
+  public int compNumber() {
+    return active;
+  }
+
+  public int find(int v) {
+    return goToRoot(v);
+  }
+
+  // returns true if they were in one component
+  public boolean union(int v1, int v2) {
+    int part1 = find(v1);
+    int part2 = find(v2);
+
+    if (part1 == part2) {
+      return true;
+    } else {
+      if (sizeList[part1] < sizeList[part2])
+        mergeParts(part1, part2);
+      else
+        mergeParts(part2, part1);
+      return false;
+    }
+  }
+
+  private int goToRoot(int v) {
+    int active = v;
+    ArrayList<Integer> visited = new ArrayList<>();
+    while (active != linkList[active]) {
+      visited.add(active);
+      active = linkList[active];
+    }
+
+    for (int i = 0; i < visited.size() - 1; i++) {
+      linkList[visited.get(i)] = active;
+    }
+
+    return active;
+  }
+
+  private void mergeParts(int from, int to) {
+    linkList[from] = to;
+    sizeList[to] += sizeList[from];
+    active--;
+  }
+}
+
+class Edge implements Comparable<Edge> {
+  public int from;
+  public int to;
+  public int weight;
+
+  public Edge(int from, int to, int weight) {
+    this.from = from;
+    this.to = to;
+    this.weight = weight;
+  }
+
+  @Override
+  public int compareTo(Edge o) {
+    return weight - o.weight;
+  }
+}
+
 public class Main {
   private static final Parser in = new Parser(System.in);
 
   public static void main(String[] args) {
+    int vertexNum = in.nextInt();
+    int edgeNum = in.nextInt();
 
+    PriorityQueue<Edge> sortedEdges = new PriorityQueue<>();
+    for (int i = 0; i < edgeNum; i++) {
+      sortedEdges.add(new Edge(in.nextInt() - 1, in.nextInt() - 1, in.nextInt()));
+    }
+
+    DSU dsu = new DSU(vertexNum);
+    int weightCounter = 0;
+
+    while (dsu.compNumber() > 1) {
+      Edge currentEdge = sortedEdges.poll();
+      if (!dsu.union(currentEdge.from, currentEdge.to)) {
+        weightCounter += currentEdge.weight;
+      }
+    }
+
+    System.out.println(weightCounter);
   }
 }
+
