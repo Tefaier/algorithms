7ebd2f962b6bdeba7d3f2dcfaa51932acf4dbff5
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-04-16 17:20:10 +0300

    Task09_B - more methods

--- src/main/java/Task09/Task09_B/Main.java
+++ src/main/java/Task09/Task09_B/Main.java
@@ -1,13 +1,17 @@
 package Task09.Task09_B;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class Main {
   private static Parser in = new Parser(System.in);
 
   public static void main(String[] args) {
-
+    String str1 = "abanabanaband";
+    String str2 = "aaa";
+    System.out.println(StringHandler.getPeriod(str1));
   }
 }
 
@@ -103,5 +107,84 @@ class Parser {
       fillBuffer();
     }
     return buffer[bufferPointer++];
+  }
+}
+
+class StringHandler {
+  public static int[] prefixFunction(StringBuilder suffixString, String prefixString, boolean endPriotity) {
+    int[] start = new int[prefixString.length()];
+
+    for (int i = 1; i < start.length; ++i) {
+      int current = start[i - 1];
+      while (prefixString.charAt(i) != prefixString.charAt(current) && current > 0)
+        current = start[current - 1];
+      if (prefixString.charAt(i) == prefixString.charAt(current))
+        start[i] = current + 1;
+    }
+    if (suffixString == null) return start;
+
+    int[] end = new int[endPriotity ? Math.min(suffixString.length(), prefixString.length()) : suffixString.length()];
+    int from = suffixString.length() - end.length;
+    end[0] = suffixString.charAt(from) == prefixString.charAt(0) ? 1 : 0;
+    for (int i = from + 1; i < suffixString.length(); ++i) {
+      int current = end[i - from - 1];
+      while ((current == prefixString.length() || suffixString.charAt(i) != prefixString.charAt(current)) && current > 0)
+        current = start[current - 1];
+      if (suffixString.charAt(i) == prefixString.charAt(current))
+        end[i - from] = current + 1;
+    }
+
+    return end;
+  }
+
+  public static int[] zFunction(String functionString, String prefixString) {
+    int[] start = new int[prefixString.length()];
+
+    int left = 0;
+    int right = 0;
+    for (int i = 1; i < start.length; ++i) {
+      start[i] = Math.max(0, Math.min(right - i, start[i - left]));
+      while (start[i] + i < prefixString.length() && prefixString.charAt(start[i]) == prefixString.charAt(start[i] + i))
+        ++start[i];
+      if (start[i] + i > right) {
+        left = i;
+        right = start[i] + i;
+      }
+    }
+    if (functionString == null) return start;
+
+    int[] end = new int[functionString.length()];
+    left = 0;
+    right = 0;
+    for (int i = 0; i < functionString.length(); ++i) {
+      end[i] = Math.max(0, Math.min(right - i, (i - left >= start.length) ? 0 : start[i - left]));
+      while (end[i] + i < functionString.length() && end[i] < prefixString.length() && prefixString.charAt(end[i]) == functionString.charAt(end[i] + i))
+        ++end[i];
+      if (end[i] + i > right) {
+        left = i;
+        right = end[i] + i;
+      }
+    }
+    return end;
+  }
+
+  public static int getPeriod(String str) {
+    int[] z = new int[str.length()];
+
+    int left = 0;
+    int right = 0;
+    for (int i = 1; i < z.length; ++i) {
+      z[i] = Math.max(0, Math.min(right - i, z[i - left]));
+      while (z[i] + i < str.length() && str.charAt(z[i]) == str.charAt(z[i] + i))
+        ++z[i];
+      if (z[i] + i > right) {
+        left = i;
+        right = z[i] + i;
+        if (right == z.length) {
+          return i;
+        }
+      }
+    }
+    return z.length;
   }
 }
